90cf9194694d7c6cbd039585c85706a1c35e8bb9
michaelsp <michael.npsp@gmail.com>
2021-08-27 03:47:47 +0200

    Now minimap icons can be "boxed". Added configuration popupmenu.

--- KiwiMBC.lua
+++ KiwiMBC.lua
@@ -4,22 +4,22 @@ local addon = CreateFrame('Frame')
 addon.addonName = ...
 
 --- upvalues
-
 local ipairs = ipairs
+local unpack = unpack
 local strfind = strfind
 local GetTime = GetTime
 local C_Timer_After = C_Timer.After
 
---- upvalues
-
+--- savedvariables defaults
 local defaults = {
 	hide = { clock = false, zoom = false, time = false, zone = false, toggle = false, worldmap = false },
+	boxed = {},         -- boxed buttons
+	alwaysVisible = {}, -- always visible buttons
+	minimapIcon = {},
 }
 
 --- frames to ignore in minimap button collection
-
 local Ignore = {
-	"Questie",
 	ActionBar = true,
 	BonusActionButton = true,
 	MainMenu = true,
@@ -58,7 +58,7 @@ local Ignore = {
 	UIParent = true,
 	MinimapCluster = true,
 	MinimapButtonFrameDragButton = true,
-	MiniMapTracking = true,
+--	MiniMapTracking = true,
 	MiniMapVoiceChatFrame = true,
 	MiniMapWorldMapButton = true,
 	MiniMapLFGFrame = true,
@@ -67,17 +67,30 @@ local Ignore = {
 	MiniMapMailFrame = true,
 	MiniMapBattlefieldFrame = true,
 	GameTimeFrame = true,
-	FeedbackUIButton =true,
+	FeedbackUIButton = true,
 	MiniMapTrackingFrame = true
 }
 
+-- savedvariables
 local cfg
+
+-- KiwiMBC minimap button
+local kiwiButton
+
+-- all minimap button names sorted
+local sortedButtons = {}
+
+-- standard minimap buttons
 local minimapButtons = {}
 local insideMinimap = false
 local dragStart = false
 local timerActive = false
 local delayHide = .5
 local dealyShow = .5
+
+-- boxed minimap buttons
+local boxedButtons = {}
+local boxedVisible = false
 
 --- utils
 local function CopyTable(src, dst)
@@ -92,17 +105,100 @@ local function CopyTable(src, dst)
 	return dst
 end
 
+local function RemoveTableValue(t,v)
+	for i=#t,1,-1 do
+		if t[i]==v then
+			table.remove(t,i)
+			return true
+		end
+	end
+end
+
+local function SkinButton(button)
+	for _,tex in ipairs({button:GetRegions()}) do
+		if tex:IsObjectType('Texture') and tex:GetDrawLayer()=='OVERLAY' then
+			tex:SetVertexColor(0.15,0.15,0.15,1)
+			return
+		end
+	end
+end
+
+local function GetButtonHumanName(buttonName)
+	if buttonName=='MiniMapTracking' then
+		return 'Tracking'
+	else
+		local name = string.match(buttonName,'^LibDBIcon10_(.+)$') or gsub(buttonName,'MinimapButton','')
+		name = gsub( name, '[_-]', ' ' )
+		return gsub( name, 'Broker', '' )
+	end
+end
+
+-- boxed buttons management
+local function Boxed_BoxButton(button)
+	local data = {}
+	for i=1,button:GetNumPoints() do
+		data[i] = { button:GetPoint(i) }
+	end
+	button.__kbmcSavedPosition = data
+	local name = button:GetName()
+	boxedButtons[name]   = button
+	minimapButtons[name] = nil
+	local boxed = cfg.boxed
+	if not boxed[name] then
+		boxed[name] = true
+		table.insert( boxed, name )
+	end
+	button:SetShown(boxedVisible)
+end
+
+local function Boxed_UnboxButton(button)
+	button:ClearAllPoints()
+	for _,points in ipairs(button.__kbmcSavedPosition) do
+		button:SetPoint( unpack(points) )
+	end
+	button.__kbmcSavedPosition = nil
+	local name = button:GetName()
+	boxedButtons[name] = nil
+	minimapButtons[name] = button
+	local boxed = cfg.boxed
+	boxed[name] = nil
+	RemoveTableValue( boxed, name )
+end
+
+local function Boxed_LayoutButtons()
+	local prevButton = kiwiButton
+	for i,name in ipairs(cfg.boxed) do
+		local button = boxedButtons[name]
+		button:ClearAllPoints()
+		button:SetPoint('TOP',prevButton,'BOTTOM',0,4)
+		prevButton = button
+	end
+end
+
+local function Boxed_ToggleVisibility()
+	boxedVisible = not boxedVisible
+	for _,name in ipairs(cfg.boxed) do
+		boxedButtons[name]:SetShown(boxedVisible)
+	end
+end
+
 --- minimap buttons visibility control
 local UpdateButtonsVisibility, UpdateButtonsVisibilityDelayed
 do
 	function UpdateButtonsVisibility()
 		timerActive = false
-		if not dragStart then
-			for button in pairs(minimapButtons) do
-				button:SetShown(insideMinimap)
+		if not dragStart and not IsMouseButtonDown() then
+			local alwaysVisible = cfg.alwaysVisible
+			for buttonName, button in pairs(minimapButtons) do
+				if insideMinimap or not boxedVisible or button~=kiwiButton then
+					button:SetShown( insideMinimap or alwaysVisible[buttonName] )
+				end
 			end
+		else
+			UpdateButtonsVisibilityDelayed()
 		end
 	end
+
 	function UpdateButtonsVisibilityDelayed()
 		if not timerActive then
 			timerActive = GetTime()
@@ -126,24 +222,27 @@ local function MinimapOnLeave(f)
 	end
 end
 
-local function MinimapDragStart()
+local function MinimapDragStart(button)
 	dragStart = true
 end
 
-local function MinimapDragStop()
+local function MinimapDragStop(button)
 	dragStart = false
 	UpdateButtonsVisibilityDelayed()
+	if boxedButtons[button:GetName()] then
+		Boxed_LayoutButtons()
+	end
 end
 
 -- collect buttons from minimap
-local CollectMinimapButtons, CollectSavedMinimapButtons
+local CollectMinimapButtons
 do
-	local function AddMinimapButton(f, name)
-		minimapButtons[f] = true
-		f:HookScript('OnEnter', MinimapOnEnter)
-		f:HookScript('OnLeave', MinimapOnLeave)
-		f:HookScript("OnDragStart", MinimapDragStart)
-		f:HookScript("OnDragStop", MinimapDragStop)
+	local function AddMinimapButton(button, name)
+		minimapButtons[name] = button
+		button:HookScript('OnEnter', MinimapOnEnter)
+		button:HookScript('OnLeave', MinimapOnLeave)
+		button:HookScript("OnDragStart", MinimapDragStart)
+		button:HookScript("OnDragStop", MinimapDragStop)
 	end
 	local function IsValidButtonName(name)
 		if name and name~='' and not Ignore[name] then
@@ -156,11 +255,17 @@ do
 		end
 	end
 	local function CollectFrameButtons(frame)
-		for _, f in ipairs({frame:GetChildren()}) do
-			if not minimapButtons[f] and f:HasScript('OnClick') and f:IsShown() then
-				local name = f:GetName()
+		for _, button in ipairs({frame:GetChildren()}) do
+			local name = button:GetName()
+			if not minimapButtons[name] and not boxedButtons[name] and button:HasScript('OnClick') and button:IsShown() then
 				if IsValidButtonName(name) then
-					AddMinimapButton(f, name)
+					if cfg.boxed[name] then
+						Boxed_BoxButton(button)
+					else
+						AddMinimapButton(button, name)
+					end
+					table.insert(sortedButtons, name)
+					SkinButton(button)
 				end
 			end
 		end
@@ -176,6 +281,7 @@ local function UpdateMinimapButtons()
 	UpdateButtonsVisibility()
 end
 
+-- blizzard buttons visibility
 local UpdateBlizzardVisibility
 do
 	local function Hide( name, frame )
@@ -204,18 +310,45 @@ addon:SetScript("OnEvent", function(frame, event, name)
 		addon.__loaded = true
 	end
 	if addon.__loaded and IsLoggedIn() then
-		KiwiMBCDB = KiwiMBCDB or CopyTable(defaults)
+		KiwiMBCDB = CopyTable(defaults, KiwiMBCDB )
 		cfg = KiwiMBCDB
 		addon:UnregisterAllEvents()
+		addon.minimapLib:Register("KiwiMBC", addon.minimapLDB, cfg.minimapIcon)
+		kiwiButton = addon.minimapLib:GetMinimapButton('KiwiMBC')
 		Minimap:HookScript('OnEnter', MinimapOnEnter)
 		Minimap:HookScript('OnLeave', MinimapOnLeave)
 		delayHide = cfg.delayHide or 0.5
 		delayShow = cfg.delayShow or 0.5
 		UpdateBlizzardVisibility()
 		C_Timer_After( .05, UpdateMinimapButtons )
-		C_Timer_After( 3,   UpdateMinimapButtons )
+		C_Timer_After( 3, function()
+			UpdateMinimapButtons()
+			Boxed_LayoutButtons()
+		end)
 	end
 end)
+
+-- minimap&ldb button
+do
+	addon.minimapLDB = LibStub("LibDataBroker-1.1", true):NewDataObject("KiwiMBC", {
+		type  = "launcher",
+		label = GetAddOnInfo("KiwiMBC", "Title"),
+		icon = "Interface\\Addons\\KiwiMBC\\icon",
+		OnClick = function(self, button)
+			if button=="LeftButton" then
+				Boxed_ToggleVisibility()
+			elseif button=="RightButton" then
+				addon:ShowPopupMenu()
+			end
+		end,
+		OnTooltipShow = function(tooltip)
+			tooltip:AddLine("KiwiMBC")
+			tooltip:AddLine("Minimap Button Controller", 1, 1, 1)
+			tooltip:AddLine("|cFFff4040Left Click|r to display boxed buttons\n|cFFff4040Right Click|r to open menu", 0.2, 1, 0.2)
+		end,
+	})
+	addon.minimapLib = LibStub("LibDBIcon-1.0")
+end
 
 --config
 SLASH_KIWIMBC1,SLASH_KIWIMBC2 = "/kmbc","/kiwimbc";
@@ -240,7 +373,6 @@ SlashCmdList.KIWIMBC = function(args)
 		print("  /kmbc toggle   -minimap toggle button visibility")
 		print("  /kmbc worldmap -worldmap button visibility")
 		print("  /kmbc delay [1-10] [1-10] - [hide] [show] delay in tenths of a second")
-		print("  /kmbc remember - toggle remember collected buttons between sessions")
 	end
 	print("KiwiMBC setup:")
 	for name in pairs(defaults.hide) do
@@ -250,3 +382,69 @@ SlashCmdList.KIWIMBC = function(args)
 	print( string.format('  buttons hide delay: %.1f tenths of a second', (cfg.delayHide or 0.5)*10 ) )
 end
 
+-- popup menu
+do
+	local menuFrame = CreateFrame("Frame", "KiwiMBCDPopupMenu", UIParent, "UIDropDownMenuTemplate")
+	local menuBoxed = {}
+	local menuAlways = {}
+	-- blizzard buttons
+	local function BlizGet(info)
+		return not cfg.hide[info.value]
+	end
+	local function BlizSet(info)
+		cfg.hide[info.value] = not cfg.hide[info.value]
+		UpdateBlizzardVisibility()
+	end
+	local menuTable = {
+		{ text = 'KiwiMBC',          notCheckable= true, isTitle = true },
+		{ text = 'Blizzard Buttons', notCheckable= true, hasArrow = true, menuList = {
+			{ text='Zone',      value='zone',     isNotRadio=true, keepShownOnClick=1, checked=BlizGet, func=BlizSet },
+			{ text='Clock',     value='clock',    isNotRadio=true, keepShownOnClick=1, checked=BlizGet, func=BlizSet },
+			{ text='Zoom',      value='zoom',     isNotRadio=true, keepShownOnClick=1, checked=BlizGet, func=BlizSet },
+			{ text='Time',      value='time',     isNotRadio=true, keepShownOnClick=1, checked=BlizGet, func=BlizSet },
+			{ text='Toggle',    value='toggle',   isNotRadio=true, keepShownOnClick=1, checked=BlizGet, func=BlizSet },
+			{ text='World Map', value='worldmap', isNotRadio=true, keepShownOnClick=1, checked=BlizGet, func=BlizSet },
+		} },
+		{ text = 'Always Visible',   notCheckable= true, hasArrow = true, menuList = menuAlways },
+		{ text = 'Boxed Buttons',    notCheckable= true, hasArrow = true, menuList = menuBoxed },
+	}
+	-- boxed buttons
+	local nonBoxedButtons = { LibDBIcon10_KiwiMBC = true, MiniMapTracking = true }
+	local function BoxedGet(info)
+		return cfg.boxed[info.value]
+	end
+	local function BoxedSet(info)
+		if cfg.boxed[info.value] then
+			Boxed_UnboxButton( boxedButtons[info.value] )
+		else
+			Boxed_BoxButton( minimapButtons[info.value] )
+		end
+		 Boxed_LayoutButtons()
+	end
+	-- always visible buttons
+	local function AlwaysGet(info)
+		return cfg.alwaysVisible[info.value]
+	end
+	local function AlwaysSet(info)
+		cfg.alwaysVisible[info.value] = not cfg.alwaysVisible[info.value] or nil
+		UpdateButtonsVisibility()
+	end
+	-- main
+	local function ButtonAddItem(buttonName)
+		local name = GetButtonHumanName(buttonName)
+		table.insert(menuAlways, {text=name, value=buttonName, isNotRadio=true, keepShownOnClick=1, checked=AlwaysGet, func=AlwaysSet} )
+		if not nonBoxedButtons[buttonName] then
+			table.insert(menuBoxed, {text=name, value=buttonName, isNotRadio=true, keepShownOnClick=1, checked=BoxedGet, func=BoxedSet} )
+		end
+	end
+	function addon:ShowPopupMenu()
+		addon.ShowPopupMenu = function()
+			EasyMenu(menuTable, menuFrame, 'cursor', 0 , 0, "MENU", 1)
+		end
+		table.sort(sortedButtons)
+		for _,buttonName in ipairs(sortedButtons) do
+			ButtonAddItem(buttonName)
+		end
+		addon:ShowPopupMenu()
+	end
+end
